ac9e36153549e08dac11d4e74e996608bdafc1bb
Rishit Toteja <77073932+RishitToteja@users.noreply.github.com>
2022-06-09 23:02:46 +0530

    Time: 169 ms (79.14%), Space: 15.9 MB (48.32%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,5 +1,3 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        from itertools import combinations
-        l = [i for i in range(1, n+1)]
-        return list(combinations(l, k))
+        return list(combinations([i for i in range(1, n+1)], k))
