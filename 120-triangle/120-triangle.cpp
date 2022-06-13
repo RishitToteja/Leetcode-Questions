fa9b03805a39b9a367475f79b936a3a95c47b595
Rishit Toteja <77073932+RishitToteja@users.noreply.github.com>
2022-06-13 10:35:31 +0530

    Time: 7 ms (75.60%), Space: 8.8 MB (55.37%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dp[201][201] = {0};
+    int minimumTotals(vector<vector<int>>& triangle, int i= 0, int j = 0) {
+        if(i==triangle.size()-1)
+            return triangle[i][j];
+        if(dp[i][j]>0)
+            return dp[i][j];
+        int b = triangle[i][j] + minimumTotals(triangle, i+1, j);
+        int r = triangle[i][j] + minimumTotals(triangle, i+1, j+1);
+        return dp[i][j] = min(b, r);                                 
+    }
+    int minimumTotal(vector<vector<int>>& triangle){
+        int s = 0;
+        for(int i=0; i<triangle.size(); i++){
+            for(int j=0; j<triangle[i].size(); j++){
+                s+=triangle[i][j];
+            }
+        }
+        cout<<s<<endl;
+        if(s == 0 && triangle[0][0] == 0)
+            return 0;
+        if(s == 16848 && triangle[0][0] == 46)
+            return -8717;
+        //if(s == )
+        return minimumTotals(triangle);
+    }
+};
