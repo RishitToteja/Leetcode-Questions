e04d8c4e3c14c05367453785c69ea5695b6197b7
Rishit Toteja <77073932+RishitToteja@users.noreply.github.com>
2022-06-28 17:04:27 +0530

    Time: 218 ms (8.19%), Space: 22 MB (5.53%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
-    void print_map(unordered_map<char, int>pq){
-        for(auto&i: pq){
-            cout<<i.first<<": "<<i.second<<endl;
-        }
-        cout<<endl;
-    }
     pair<bool,char> check(unordered_map<char, int>pq){
         unordered_map<int, vector<char>>m;
-        for(auto&i: pq){
-            m[i.second] = {};
-        }
         for(auto&i: pq){
             m[i.second].push_back(i.first);
         }
@@ -27,7 +18,6 @@ public:
         }
         int count = 0;
         while(true){
-            print_map(pq);
             pair<bool,char>t = check(pq);
             if(t.first)
                 break;
