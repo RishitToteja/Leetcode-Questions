c8211a7751b4dffbe5a7e6c803809403a2ef970d
Rishit Toteja <77073932+RishitToteja@users.noreply.github.com>
2022-07-22 12:11:08 +0530

    Time: 1083 ms (9.99%), Space: 57.7 MB (5.85%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int *dp;
+    Solution(){
+        dp = new int[10001];
+        for(int i=0; i<10001; i++){
+            dp[i]=-1;
+        }
+    }
+    bool canJump(vector<int>& nums, int idx = 0) {
+        if(idx == nums.size()-1)
+            return true;
+        if(dp[idx] !=-1){
+            return dp[idx];
+        }
+        
+        int maxJump = nums[idx];
+        
+        for(int i=1; i<=maxJump; i++){
+            bool bol = canJump(nums, idx+i);
+            if(bol)
+                return dp[idx]=true;
+        }
+        return dp[idx]=false;
+    }
+};
