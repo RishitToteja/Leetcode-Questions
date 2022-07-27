7a0212dec93b5393b3d468491152519386ea4dfe
Rishit Toteja <77073932+RishitToteja@users.noreply.github.com>
2022-07-27 21:02:48 +0530

    Time: 151 ms (84.34%), Space: 93.7 MB (9.22%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -3,24 +3,29 @@ public:
     int maxProfit(vector<int>& prices) {
         
         pair<int,int> maximum = {0, -1}; 
-        pair<int,int> minimum = {prices[0], 0};
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minimum;
+        minimum.push({prices[0], 0});
         
         for(int i = 1; i<prices.size(); i++){
             
-            if(prices[i]-minimum.first>maximum.first){
-                maximum.first = prices[i]- minimum.first;
+            if(prices[i]-minimum.top().first>maximum.first){
+                maximum.first = prices[i]- minimum.top().first;
                 maximum.second = i;
-            }
-                
-            
-            if(prices[i]<minimum.first){
-                minimum.first = prices[i];
-                minimum.second = i;
+
+            }            
+            if(prices[i]<minimum.top().first){
+                minimum.push({prices[i], i});
             }
         }
-        cout<<"Buy on: "<<minimum.second<<endl;
+        while(!minimum.empty() && minimum.top().second>maximum.second){
+            minimum.pop();
+            if(minimum.empty())
+                return 0;
+        }
+        
+        cout<<"Buy on: "<<minimum.top().second<<endl;
         cout<<"Sell on: "<<maximum.second<<endl;
         
-        return maximum.first;
+        return (prices[maximum.second] - prices[minimum.top().second]);
     }
 };
