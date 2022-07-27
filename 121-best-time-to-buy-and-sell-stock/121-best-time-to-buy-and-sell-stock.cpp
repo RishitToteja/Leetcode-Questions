ce7b179e44aa0ca09808742ed767e35f10c9f9ce
Rishit Toteja <77073932+RishitToteja@users.noreply.github.com>
2022-07-27 18:41:06 +0530

    Time: 245 ms (21.81%), Space: 93.5 MB (11.99%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -2,18 +2,25 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-        int maximum = 0; 
-        int minimum = prices[0];
+        pair<int,int> maximum = {0, -1}; 
+        pair<int,int> minimum = {prices[0], 0};
         
         for(int i = 1; i<prices.size(); i++){
             
-            if(prices[i]-minimum>maximum)
-                maximum = prices[i]- minimum;
+            if(prices[i]-minimum.first>maximum.first){
+                maximum.first = prices[i]- minimum.first;
+                maximum.second = i;
+            }
+                
             
-            if(prices[i]<minimum)
-                minimum = prices[i];
+            if(prices[i]<minimum.first){
+                minimum.first = prices[i];
+                minimum.second = i;
+            }
         }
+        cout<<"Buy on: "<<minimum.second<<endl;
+        cout<<"Sell on: "<<maximum.second<<endl;
         
-        return maximum;
+        return maximum.first;
     }
 };
