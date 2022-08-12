29bd89acd796cc5658ff56e6b1fdf8fadddb6efb
Rishit Toteja <77073932+RishitToteja@users.noreply.github.com>
2022-08-12 17:43:39 +0530

    Time: 31 ms (93.09%), Space: 23.3 MB (63.86%) - LeetHub

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    void helper(TreeNode*root, int v, vector<int>&arr){
+        if(root==NULL){
+            return ;
+        }
+        else{
+            if(root->val == v){
+                arr.push_back(root->val);
+                return ;
+            }
+            else if(root->val< v){
+                helper(root->right, v, arr);
+                arr.push_back(root->val);
+                return ;
+            }
+            else{
+                helper(root->left, v, arr);
+                arr.push_back(root->val);
+                return ;
+            }
+        }
+        
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<int>a;
+        helper(root, p->val, a);
+        
+        vector<int>b;
+        helper(root, q->val, b);
+        
+        map<int, int>mp;
+        for(int i=0; i<a.size(); i++){
+            mp[a[i]]++;
+        }
+        for(int i=0; i<b.size(); i++){
+            if(mp.find(b[i]) != mp.end()){
+                TreeNode*n = new TreeNode(b[i]);
+                return n;
+            }
+        }
+        
+        return nullptr;
+        
+        
+    }
+};
