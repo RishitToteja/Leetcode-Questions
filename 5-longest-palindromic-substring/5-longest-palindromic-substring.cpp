f7bd9b95466b2d83f3eac2d02e837d7ab765432c
Rishit Toteja <77073932+RishitToteja@users.noreply.github.com>
2022-08-23 15:01:35 +0530

    Time: 708 ms (19.39%), Space: 283 MB (13.89%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,57 +1,58 @@
 class Solution {
 public:
-    unordered_map<string, bool>mp;
-    bool check(string s){
-        if(mp.find(s)!= mp.end())
-            return mp[s];
-        int i=0;
-        int j=s.size()-1;
-        while(i<=j){
-            if(s[i]!=s[j]){
-                mp[s]=false;
-                return mp[s];
-            }
-                
-            i++;
-            j--;
-        }
-        mp[s]=true;
-        return mp[s];
-    }
-    string longestPalindrome(string s) {        
-        int t = s.size();
-        cout<<t<<endl;
-        if(check(s))
-            return s;
-        if(t==1)
-            return s;
-        if(t==1000 && s[0]=='a'){
-            return "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-        }
-        string temp = "";
-        temp+=s[0];
-        pair<string, int> ans = {temp, 1};
+
+    string longestPalindrome(string s) {
+        int n = s.size();
+        vector<vector<int>>dp(n, vector<int>(n, -1));
         
-        for(int i=0; i<s.size(); i++){
-            pair<string, int> m = {"", 0};
-            int j = s.size()-1;
-            
-            while(i<j){
-                if(s[i]==s[j] && check(s.substr(i+1, j-i-1))){
-                    if(j-i+1 > m.second)
-                        m = {s.substr(i, j-i+1), j-i+1};
-                    else j--;
-                    }
-                else{
-                    j--;
-                }
-                    
+        pair<int, pair<int, int>>p = {INT_MIN, {-1, -1}};
+        for(int i=0; i<n; i++){
+            dp[i][i] = 1;
+            if(1>p.first){
+                    p = {1, {i, i}};
             }
-            if(m.second>ans.second)
-                ans = {m.first, m.second};
-                
         }
-                
-      return ans.first;
+        
+        for(int i=0; i<n-1; i++){
+            if(s[i]==s[i+1]){
+                dp[i][i+1] = 1;
+                if(2>p.first){
+                    p = {2, {i, i+1}};
+                }
+            }
+            else
+                dp[i][i+1] = 0;
+        }
+        int j = 2;
+        
+        while(j<n){
+            for(int i=0; i<n-j; i++){
+                if(s[i]==s[i+j] && dp[i+1][i+j-1]==1){
+                    dp[i][i+j]=1;
+                    if(j+1>p.first){
+                        p = {j+1, {i, j+i}};
+                    }
+                }
+                else{
+                    dp[i][i+j]=0;
+                }
+            }
+            j++;
+        }
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<n; j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        string se = "";
+        for(int i=p.second.first; i<=p.second.second; i++){
+            se += s[i];
+        }
+        return se;
     }
 };
+
+// 0, 1
+// 1, 2
+// 2, 3
