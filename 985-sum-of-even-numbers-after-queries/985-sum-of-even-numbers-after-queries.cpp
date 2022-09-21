fd9c8c15f2d5b4a40c5ffffcf541f46e1eef52d8
Rishit Toteja <77073932+RishitToteja@users.noreply.github.com>
2022-09-21 19:16:21 +0530

    Time: 121 ms (86.71%), Space: 46.1 MB (66.47%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -3,22 +3,15 @@ public:
     vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
         vector<int>ans;
         int sum = 0;
-        for(int i=0; i<nums.size();i++){
+        for(int i=0; i<nums.size();i++)
             if(!(nums[i]&1))
                 sum += nums[i];
-        }
         for(int i=0; i<queries.size(); i++){
-            int val = queries[i][0];
-            int idx  = queries[i][1];
-            int new_val = nums[idx] + val;
-            if(!(nums[idx]&1)){
-                sum -= nums[idx]; 
-            }
-            if(!(new_val&1)){
-                sum += new_val;
-            }
-            nums[idx] = new_val;
-            
+            if(!(nums[queries[i][1]]&1))
+                sum -= nums[queries[i][1]]; 
+            if(!((nums[queries[i][1]] + queries[i][0])&1))
+                sum += (nums[queries[i][1]] + queries[i][0]);
+            nums[queries[i][1]] = (nums[queries[i][1]] + queries[i][0]);
             ans.push_back(sum);
         }
         return ans;
