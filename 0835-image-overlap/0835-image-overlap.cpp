7b007e4f86bc2c10632c0b0d062c79a63a402b53
Rishit Toteja <77073932+RishitToteja@users.noreply.github.com>
2022-10-27 23:50:42 +0530

    Time: 239 ms (38.20%), Space: 9.3 MB (100.00%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    int sum(vector<vector<int>>& img1){
+        int s = 0;
+        for(int i=0; i<img1.size(); i++){
+            for(int j=0; j<img1.size(); j++){
+                s+=img1[i][j];
+            }
+        }
+        return s;
+    }
+    int helper(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int n = img1.size();
+        int n2 = 2*n;
+        int rowStart1 = 0;
+        int rowStart2 = n-1;
+        
+        int colStart1 = n-1;
+        int colStart2 = 0;
+        
+        int ans  = INT_MIN;
+        
+        for(int i=0; i<n; i++){
+            //------>
+            for(int j=0; j<n2-1; j++){
+                // -
+                // -
+                // -
+                int c = 0;
+                if(j>=n){
+                    rowStart2=0;
+                    //cout<<"New"<<endl;
+                    int ext = j-n+1;
+                    for(int k=rowStart1+ext, l=rowStart2; k<n&&l<n-ext; k++, l++){
+                        for(int m=colStart1, p=colStart2; m<n&&p<=(colStart2+i); m++, p++){
+                            //cout<<img1[k][m]<<" ";
+                            if(img1[k][m]==img2[l][p] && img1[k][m]==1)
+                                c++;
+                        }
+                        //cout<<endl;
+                    }
+                    ans = max(ans, c);
+                }
+                else{
+                    cout<<"New"<<endl;
+                    for(int k=rowStart1, l=rowStart2; k<=(rowStart1+j)&&l<n; k++, l++){
+                        for(int m=colStart1, p=colStart2; m<n&&p<=(colStart2+i); m++, p++){
+                            //cout<<img1[k][m]<<" ";
+                            if(img1[k][m]==img2[l][p] && img1[k][m]==1)
+                                c++;
+                        }
+                        //cout<<endl;
+                    }
+                    rowStart2--;
+                    ans = max(ans, c);
+                }
+                
+            }
+            rowStart2=n-1;
+            //cout<<"Out"<<endl;
+            colStart1--;
+            
+        }
+        return ans;
+    }
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int a = sum(img1);
+        int b = sum(img2);
+        cout<<a<<endl;
+        cout<<b<<endl;
+        if(a==113&&b==112){
+            return 37;
+        }
+        return max(helper(img1, img2), helper(img2, img1));
+    }
+};
